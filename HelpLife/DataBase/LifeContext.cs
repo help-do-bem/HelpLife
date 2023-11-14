8c949317d380e3de56e10ef1120e69e37b6a32b7
JulioMachadoFretes <101196604+JulioFretes@users.noreply.github.com>
2023-11-13 23:08:13 -0300

    First Commit

---
+++ HelpLife/DataBase/LifeContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace HelpLife.DataBase
+{
+    public class LifeContext : IdentityDbContext<IdentityUser>
+    {
+    }
+}
